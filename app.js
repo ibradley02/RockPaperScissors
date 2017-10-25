fa18e9ffd81a694f02b58967aeb9dcfa4e5ac99c
Ian Bradley <ianbradleydebate@gmail.com>
2017-10-25 16:04:35 -0600

    And now it works

--- app.js
+++ app.js
@@ -1,17 +1,37 @@
-Array.prototype.random = function() {
-    return this[Math.floor((Math.random()*this.length))];
+function compChoice() {
+    var list = ['rock', 'paper', 'scissors']
+    var randomChoice = list[Math.floor((Math.random() * list.length))];
+    var computerChoice = randomChoice
+    console.log(computerChoice)
+    return computerChoice
 }
-function play(playerChoice){
-    list = ['rock','paper','scissors']
-    var computerChoice = list.random
-    var playerChoice = document.getElementsByClassName('value')
-    function winner(playerChoice, computerChoice){
-        if(playerChoice === computerChoice){
-            console.log('TIE');
-        }else if(playerChoice === 'rock' && computerChoice === 'scissors' || playerChoice === 'scissors' && computerChoice === 'paper' || playerChoice === 'paper' && computerChoice === 'rock'){
-            console.log('YOU WIN')
+function play(playerChoice) {
+    var computerChoice = compChoice()
+    if (playerChoice === computerChoice) {
+        console.log('TIE');
+        return
+    } else if (playerChoice === 'rock') {
+        if (computerChoice === 'scissors') {
+            console.log('win')
+            return
         } else {
-            console.log('YOU LOSE')
+            console.log('lose')
+        }
+    } else if (playerChoice === 'scissors') {
+        if (computerChoice === 'paper') {
+            console.log('win')
+            return
+        } else {
+            console.log('lose')
+            return
+        }
+    } else if (playerChoice === 'paper') {
+        if (computerChoice === 'rock') {
+            console.log('win')
+            return
+        } else {
+            console.log('lose')
+            return
         }
     }
 }
